357a43bdb30562499f1e976f97ac614d98e37347
yyangflatiron <yichen.yang@flatironschool.com>
2016-06-28 14:40:53 -0400

    Done.

--- models/textanalyzer.rb
+++ models/textanalyzer.rb
@@ -7,4 +7,21 @@ class TextAnalyzer
   def text
     self.user_text
   end
+
+  def count_vowels
+    @vowels = 0
+    self.each do |letter|
+      if letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u'
+        @vowels += 1
+      end
+    end
+    @vowels
+  end
+
+  def count_consonants
+    consonants = self.length - @vowels
+  end
+
+  def most_common_letter
+  end
 end
